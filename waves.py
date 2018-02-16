9870d256ecbd09d2e411c5557bd5a43822138a58
Joseph <jojorocketn@gmail.com>
2018-02-15 21:27:14 -0500

    Fixed bug with title screen, title screen works completely.

--- waves.py
+++ waves.py
@@ -1,8 +1,7 @@
 import pygame
-
+pygame.init()
 
 #basic setup
-pygame.init()
 
 window_w = 1000
 window_h = 700
@@ -23,37 +22,73 @@ startBorder = pygame.image.load('startborder.png')
 quitButton = pygame.image.load('quit.png')
 quitBorder = pygame.image.load('quitborder.png')
 
-title_screen = True
+def title(x,y):
+    gameDisplay.blit(titleArt, (x,y))
 
-while title_screen == True:
-    
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
-    mouse = pygame.mouse.get_pos()
-    #print(mouse)
-    click = pygame.mouse.get_pressed()
-    #print(click)
+def startImg(x,y):
+    gameDisplay.blit(startButton, (x,y))
 
-    gameDisplay.fill(white)
+def quitImg(x,y):
+    gameDisplay.blit(quitButton, (x,y))
 
-    gameDisplay.blit(titleArt, (380, 50))
-    
-    gameDisplay.blit(startButton, (50, 550))
-    gameDisplay.blit(quitButton, (765, 550))
-    
-    if 50 + 185 > mouse[0] > 50 and 550 + 100 > mouse[1] > 550:
-        gameDisplay.blit(startBorder, (15, 530))
-        if click[0] == 1:
-            title_screen = False
-    if 765 + 185 > mouse[0] > 765 and 550 + 100 > mouse[1] > 550:
-        gameDisplay.blit(quitBorder, (755, 530))
-        if click[0] == 1:
-            pygame.quit()
-            quit()
+def startBrder(x,y):
+    gameDisplay.blit(startBorder, (x,y))
 
-    pygame.display.update()
-    clock.tick(60)
+def quitBrder(x,y):
+    gameDisplay.blit(quitBorder, (x,y))
 
-gameDisplay.fill(black)
+def gameIntro():
+    title_screen = True
+    while title_screen == True:
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+                
+        mouse = pygame.mouse.get_pos()
+        #print(mouse)
+        click = pygame.mouse.get_pressed()
+        #print(click)
+
+        gameDisplay.fill(white)
+
+        title(380,50)
+        startImg(50,550)
+        quitImg(765,550)
+        
+        if 50 + 185 > mouse[0] > 50 and 550 + 100 > mouse[1] > 550:
+            startBrder(15,530)
+            if click[0] == 1:
+                title_screen = False
+                
+        if 765 + 185 > mouse[0] > 765 and 550 + 100 > mouse[1] > 550:
+            quitBrder(755,530)
+            if click[0] == 1:
+                pygame.quit()
+                quit()
+
+        pygame.display.update()
+        clock.tick(60)
+
+def gameLoop():
+    end = False
+    while not end:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+                
+        gameDisplay.fill(black)
+
+        pygame.display.update()
+        clock.tick(60)
+
+gameIntro()
+gameLoop()
+
+
+
+
+
+
